b477e9a3d1c3c0e0fe305d11ab8e25e1213fe72a
Sami Konzo <samikonzo@gmail.com>
2017-10-12 15:18:39 +0300

    :slider widget create

---
+++ script/learn.js
@@ -0,0 +1,67 @@
+'use strict'
+var l = console.log
+
+
+
+document.querySelectorAll('.slider').forEach(slider => {
+	var tmp = new Slider({
+		elem: slider
+	})
+
+	$(slider).on('newValue', function(e){
+		var value = e.detail.value;
+		this.dataset.percent = value.toFixed(1) + '%';
+	})
+})
+
+function Slider(options) {
+
+	var elem = options.elem,
+		marker = elem.querySelector('.slider__marker'),
+		draggedFlag = false,
+		minLeft = 0,
+		maxLeft = $(elem).width() - $(marker).width(),
+		currentPercent = ($(marker).position().left - minLeft) / maxLeft * 100;
+
+	/*drag n drop*/
+	$(marker).on('mousedown', function(e){
+		e.preventDefault();
+		draggedFlag = true;
+		marker.offsetX = e.clientX - marker.getBoundingClientRect().left;
+	})
+
+	$(document).on('mousemove', function(e){
+		if(!draggedFlag) return
+
+		var newLeft = e.clientX - marker.offsetX;
+
+		if(newLeft > maxLeft) newLeft = maxLeft
+		if(newLeft < minLeft) newLeft = minLeft
+ 
+		marker.style.left = newLeft + 'px';
+
+
+		getPercent()
+
+		var widgetEvent = new CustomEvent('newValue',{
+			bubbles: true,
+			detail: {
+				value: currentPercent
+			}
+		})
+
+		elem.dispatchEvent(widgetEvent);
+	})
+
+	$(document).on('mouseup', function(e){
+		draggedFlag = false;
+		marker.offsetX = undefined
+	})
+	/*<----------------->*/
+
+
+	function getPercent(){
+		currentPercent = ($(marker).position().left - minLeft) / maxLeft * 100;
+		return currentPercent;
+	}
+}
